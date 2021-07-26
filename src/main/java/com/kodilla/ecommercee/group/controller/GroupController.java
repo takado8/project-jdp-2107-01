4b3ca202055c1a8dc85934accc20d46781bff34e
Marta Górska <magorska.95@gmail.com>
2021-07-26 10:37:56 +0200

    JDP210701-2 GroupController initial implementation

---
+++ src/main/java/com/kodilla/ecommercee/group/controller/GroupController.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.group.controller;
+
+import com.kodilla.ecommercee.group.domain.GroupDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/groups")
+public class GroupController {
+
+    @GetMapping(path = "getAllGroups")
+    public List<GroupDto> getAllGroups() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(path = "getGroup")
+    public GroupDto getGroup(Long groupId) {
+        return new GroupDto(1L, "test_name");
+    }
+
+    @PostMapping(path = "createGroup")
+    public void createGroup(GroupDto groupDto) {
+
+    }
+
+    @PutMapping(path = "updateGroup")
+    public GroupDto updateGroup(GroupDto groupDto) {
+        return new GroupDto(1L, "test_name");
+    }
+}
