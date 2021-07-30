61ba56697fe67be2b8b14e77ddfa889e9e8c04a8
Monika Borkowska <monikademianiuk@gmail.com>
2021-07-30 21:15:05 +0200

    Entity Group Test

--- src/test/java/com/kodilla/ecommercee/group/GroupTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/group/GroupTestSuite.java
@@ -2,12 +2,7 @@ package com.kodilla.ecommercee.group;
 
 import com.kodilla.ecommercee.group.domain.Group;
 import com.kodilla.ecommercee.group.repository.GroupDao;
-import com.kodilla.ecommercee.order.domain.Order;
-import com.kodilla.ecommercee.order.repository.OrderDao;
-import com.kodilla.ecommercee.product.domain.Product;
-import com.kodilla.ecommercee.product.repository.ProductDao;
-import com.kodilla.ecommercee.user.domain.User;
-import com.kodilla.ecommercee.user.repository.UserDao;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,13 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
+
 import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Optional;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
